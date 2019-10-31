4eca0de7b6c5ac71dbb38ba9d8995da62e6e8638
Chemllei Joseph <chemllei0304@gmail.com>
2019-10-31 16:22:12 -0400

    refactored, moved some things, added save f'n

--- post.py
+++ post.py
@@ -1,6 +1,7 @@
 class post:
   
-  def __init__(self, title, sub, link):
+  def __init__(self, title, sub, link, postID):
     self.title = title
     self.sub = sub
     self.link = link
+    self.postID = postID
--- redditFrontPage.py
+++ redditFrontPage.py
@@ -2,27 +2,52 @@ import praw
 from post import post
 import webbrowser
 import tkinter
+import sys
+from tkinter import messagebox
 
 reddit = praw.Reddit()
 
 results = []
 
-for submission in reddit.front.hot(limit=50):
-  current = post(submission.title, submission.subreddit.display_name, submission.url)
-  results.append(current)
+def viewPosts():
+  results.clear()
+  postsList.configure(state="normal")
+  postsList.delete(1.0, tkinter.END)
+  
+  for submission in reddit.front.hot(limit=50):
+    current = post(submission.title,
+                   submission.subreddit.display_name,
+                   submission.url,
+                   submission.id)
+    results.append(current)
 
+  for idx, item in enumerate(results):
+    x = str(idx + 1) + " - " + item.title + " (" + item.sub + ")\n"
+    postsList.configure(state="normal")
+    postsList.insert(tkinter.END, x)
+    postsList.configure(state="disabled")
+    
 def submitButtonClicked(num):
   numField.delete(0, tkinter.END)
   webbrowser.open(results[num - 1].link)
-
-#def saveButtonClicked(num):
-#  numField2.delete(0, tkinter.END)
+    
+def saveButtonClicked(num):
+  numField2.delete(0, tkinter.END)
+  selected = reddit.submission(id = results[num - 1].postID)
+  selected.save()
+  messagebox.showinfo("Saved", "Post Saved")
 
 mainWindow = tkinter.Tk()
 mainWindow.title("Reddit Front Page")
 
-lis = tkinter.Text(mainWindow, wrap="none")
-lis.pack()
+exitButton = tkinter.Button(mainWindow, text = "Exit", command = sys.exit)
+exitButton.pack()
+
+refreshButton = tkinter.Button(mainWindow, text = "Refresh", command = viewPosts)
+refreshButton.pack()
+
+postsList = tkinter.Text(mainWindow, wrap="none")
+postsList.pack()
   
 numField = tkinter.Entry(mainWindow)
 numField.pack()
@@ -30,16 +55,11 @@ numField.pack()
 submitButton = tkinter.Button(mainWindow, text = "Submit", command = lambda: submitButtonClicked(int(numField.get())))
 submitButton.pack()
   
-#numField2 = tkinter.Entry(mainWindow)
-#numField2.pack()
-#
-#saveButton = tkinter.Button(mainWindow, text = "Submit", command = lambda: submitButtonClicked(int(numField.get())))
-#saveButton.pack()
+numField2 = tkinter.Entry(mainWindow)
+numField2.pack()
 
-for idx, i in enumerate(results):
-  x = str(idx + 1) + " - " + i.title + " (" + i.sub + ")\n"
-  lis.configure(state="normal")
-  lis.insert(tkinter.END, x)
-  lis.configure(state="disabled")
+saveButton = tkinter.Button(mainWindow, text = "Save", command = lambda: saveButtonClicked(int(numField2.get())))
+saveButton.pack()
 
+viewPosts()
 mainWindow.mainloop()
