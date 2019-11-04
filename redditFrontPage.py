7d025ccbc0eeac562e50e9db6195b264b2966d26
Chemllei Joseph <chemllei0304@gmail.com>
2019-11-04 17:42:39 -0500

    remove exitbutton, add view/fns for saved posts

--- redditFrontPage.py
+++ redditFrontPage.py
@@ -8,6 +8,7 @@ from tkinter import messagebox
 reddit = praw.Reddit()
 
 results = []
+savedResults = []
 
 def viewPosts():
   results.clear()
@@ -27,6 +28,24 @@ def viewPosts():
     postsList.insert(tkinter.END, x)
     postsList.configure(state="disabled")
     
+def viewSaved():
+  savedResults.clear()
+  postsList2.configure(state="normal")
+  postsList2.delete(1.0, tkinter.END)
+  
+  for submission in reddit.redditor('').saved():
+    current = post(submission.title,
+                   submission.subreddit.display_name,
+                   submission.url,
+                   submission.id)
+    savedResults.append(current)
+
+  for idx, item in enumerate(savedResults):
+    x = str(idx + 1) + " - " + item.title + " (" + item.sub + ")\n"
+    postsList2.configure(state="normal")
+    postsList2.insert(tkinter.END, x)
+    postsList2.configure(state="disabled")
+    
 def submitButtonClicked(num):
   numField.delete(0, tkinter.END)
   webbrowser.open(results[num - 1].link)
@@ -35,13 +54,20 @@ def saveButtonClicked(num):
   numField2.delete(0, tkinter.END)
   selected = reddit.submission(id = results[num - 1].postID)
   selected.save()
-  messagebox.showinfo("Saved", "Post Saved")
+  viewSaved()
+    
+def unsaveButtonClicked(num):
+  numField3.delete(0, tkinter.END)
+  selected = reddit.submission(id = savedResults[num - 1].postID)
+  selected.unsave()
+  viewSaved()
+    
+def openSaveButtonClicked():
+  linkx = ""
+  webbrowser.open(linkx)
 
 mainWindow = tkinter.Tk()
 mainWindow.title("Reddit Front Page")
-
-exitButton = tkinter.Button(mainWindow, text = "Exit", command = sys.exit)
-exitButton.pack()
 
 refreshButton = tkinter.Button(mainWindow, text = "Refresh", command = viewPosts)
 refreshButton.pack()
@@ -52,14 +78,27 @@ postsList.pack()
 numField = tkinter.Entry(mainWindow)
 numField.pack()
 
-submitButton = tkinter.Button(mainWindow, text = "Submit", command = lambda: submitButtonClicked(int(numField.get())))
+submitButton = tkinter.Button(mainWindow, text = "Go To Post", command = lambda: submitButtonClicked(int(numField.get())))
 submitButton.pack()
   
 numField2 = tkinter.Entry(mainWindow)
 numField2.pack()
 
-saveButton = tkinter.Button(mainWindow, text = "Save", command = lambda: saveButtonClicked(int(numField2.get())))
+saveButton = tkinter.Button(mainWindow, text = "Save Post", command = lambda: saveButtonClicked(int(numField2.get())))
 saveButton.pack()
+  
+numField3 = tkinter.Entry(mainWindow)
+numField3.pack()
+
+unsaveButton = tkinter.Button(mainWindow, text = "Unsave Post", command = lambda: unsaveButtonClicked(int(numField3.get())))
+unsaveButton.pack()
+
+openSaveButton = tkinter.Button(mainWindow, text = "Open Saved Page", command = openSaveButtonClicked)
+openSaveButton.pack()
+
+postsList2 = tkinter.Text(mainWindow, wrap="none")
+postsList2.pack()
 
 viewPosts()
+viewSaved()
 mainWindow.mainloop()
