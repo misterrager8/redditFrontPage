6115559bc72b95e646f86ecb68d8bab7729250d6
Chemllei Joseph <chemllei0304@gmail.com>
2019-11-12 18:57:13 -0500

    jython implemented for swing gui. seperate script to getposts (compat issues)

---
+++ getPosts.py
@@ -0,0 +1,24 @@
+import praw
+import csv
+
+reddit = praw.Reddit(client_id='',
+                     client_secret='',
+                     user_agent='',
+                     username='',
+                     password='')
+
+results = []
+
+def viewPosts():
+  for submission in reddit.front.hot(limit=50):
+    current = [submission.title,
+               submission.subreddit.display_name,
+               submission.url,
+               submission.id]
+    results.append(current)
+    
+  with open("hotPosts.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(results)
+      
+viewPosts()
--- redditFrontPage.py
+++ redditFrontPage.py
@@ -1,104 +1,187 @@
-import praw
-from post import post
-import webbrowser
-import tkinter
+from javax.swing import *
+from java.awt import *
 import sys
-from tkinter import messagebox
+import csv
+import webbrowser
 
-reddit = praw.Reddit()
+mouseLoc = []
+postList = []
 
-results = []
-savedResults = []
-
-def viewPosts():
-  results.clear()
-  postsList.configure(state="normal")
-  postsList.delete(1.0, tkinter.END)
-  
-  for submission in reddit.front.hot(limit=50):
-    current = post(submission.title,
-                   submission.subreddit.display_name,
-                   submission.url,
-                   submission.id)
-    results.append(current)
-
-  for idx, item in enumerate(results):
-    x = str(idx + 1) + " - " + item.title + " (" + item.sub + ")\n"
-    postsList.configure(state="normal")
-    postsList.insert(tkinter.END, x)
-    postsList.configure(state="disabled")
+class mainWindow(JFrame):
+  def __init__(self):
+    super(mainWindow, self).__init__()
+    self.initComponents()
     
-def viewSaved():
-  savedResults.clear()
-  postsList2.configure(state="normal")
-  postsList2.delete(1.0, tkinter.END)
-  
-  for submission in reddit.redditor('').saved():
-    current = post(submission.title,
-                   submission.subreddit.display_name,
-                   submission.url,
-                   submission.id)
-    savedResults.append(current)
+  def initComponents(self):
+    self.bgPanel = JPanel(mousePressed = self.bgPanelMousePressed,
+                          mouseDragged = self.bgPanelMouseDragged
+                         )
+    self.exitButton = JLabel(mouseClicked = self.exitButtonMouseClicked)
+    self.jScrollPane1 = JScrollPane()
+    self.postTable = JTable()
+    self.saveButton = JLabel(mouseEntered = self.saveButtonMouseEntered,
+                             mouseExited = self.saveButtonMouseExited
+                            )
+    self.openButton = JLabel(mouseEntered = self.openButtonMouseEntered,
+                             mouseExited = self.openButtonMouseExited,
+                             mouseClicked = self.openButtonMouseClicked
+                            )
+    self.unSaveButton = JLabel(mouseEntered = self.unSaveButtonMouseEntered,
+                             mouseExited = self.unSaveButtonMouseExited
+                            )
+    self.refreshButton = JLabel(mouseEntered = self.refreshButtonMouseEntered,
+                                mouseExited = self.refreshButtonMouseExited
+                               )
 
-  for idx, item in enumerate(savedResults):
-    x = str(idx + 1) + " - " + item.title + " (" + item.sub + ")\n"
-    postsList2.configure(state="normal")
-    postsList2.insert(tkinter.END, x)
-    postsList2.configure(state="disabled")
+    self.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+    self.setUndecorated(True)
+
+    self.exitButton.setText("X")
+    self.exitButton.setCursor(Cursor(Cursor.HAND_CURSOR))
+
+    self.postTable.setModel(table.DefaultTableModel(
+      [],
+      ["Post Title", "Sub", "URL", "ID"]
+    ))
     
-def submitButtonClicked(num):
-  numField.delete(0, tkinter.END)
-  webbrowser.open(results[num - 1].link)
+    self.postTable.getColumnModel().getColumn(0).setPreferredWidth(400)
+    self.postTable.getColumnModel().getColumn(1).setPreferredWidth(75)
+    self.postTable.getColumnModel().getColumn(2).setPreferredWidth(150)
+    self.postTable.getColumnModel().getColumn(3).setPreferredWidth(50)
     
-def saveButtonClicked(num):
-  numField2.delete(0, tkinter.END)
-  selected = reddit.submission(id = results[num - 1].postID)
-  selected.save()
-  viewSaved()
+    self.jScrollPane1.setViewportView(self.postTable)
+
+    self.saveButton.setBackground(Color(255, 255, 255))
+    self.saveButton.setHorizontalAlignment(SwingConstants.CENTER)
+    self.saveButton.setText("Save Post")
+    self.saveButton.setCursor(Cursor(Cursor.HAND_CURSOR))
+    self.saveButton.setOpaque(True)
+
+    self.openButton.setBackground(Color(255, 255, 255))
+    self.openButton.setHorizontalAlignment(SwingConstants.CENTER)
+    self.openButton.setText("Open Post")
+    self.openButton.setCursor(Cursor(Cursor.HAND_CURSOR))
+    self.openButton.setOpaque(True)
+
+    self.unSaveButton.setBackground(Color(255, 255, 255))
+    self.unSaveButton.setHorizontalAlignment(SwingConstants.CENTER)
+    self.unSaveButton.setText("unSave Post")
+    self.unSaveButton.setCursor(Cursor(Cursor.HAND_CURSOR))
+    self.unSaveButton.setOpaque(True)
+
+    self.refreshButton.setBackground(Color(255, 255, 255))
+    self.refreshButton.setHorizontalAlignment(SwingConstants.CENTER)
+    self.refreshButton.setText("Refresh Button")
+    self.refreshButton.setCursor(Cursor(Cursor.HAND_CURSOR))
+    self.refreshButton.setOpaque(True)
+
+    bgPanelLayout = GroupLayout(self.bgPanel)
+    self.bgPanel.setLayout(bgPanelLayout)
+    bgPanelLayout.setHorizontalGroup(
+      bgPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+      .addGroup(bgPanelLayout.createSequentialGroup()
+        .addContainerGap()
+        .addGroup(bgPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+          .addGroup(bgPanelLayout.createSequentialGroup()
+            .addGap(0, 0, sys.maxint)
+            .addComponent(self.exitButton))
+          .addComponent(self.jScrollPane1, GroupLayout.DEFAULT_SIZE, 529, sys.maxint)
+          .addGroup(bgPanelLayout.createSequentialGroup()
+            .addComponent(self.unSaveButton, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE)
+            .addGap(0, 0, sys.maxint))
+          .addGroup(bgPanelLayout.createSequentialGroup()
+            .addGroup(bgPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+              .addComponent(self.saveButton, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE)
+              .addComponent(self.openButton, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE))
+            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, sys.maxint)
+            .addComponent(self.refreshButton, GroupLayout.PREFERRED_SIZE, 133, GroupLayout.PREFERRED_SIZE)))
+        .addContainerGap())
+    )
+    bgPanelLayout.setVerticalGroup(
+      bgPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+      .addGroup(bgPanelLayout.createSequentialGroup()
+        .addContainerGap()
+        .addComponent(self.exitButton)
+        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(self.jScrollPane1, GroupLayout.PREFERRED_SIZE, 147, GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+        .addGroup(bgPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+          .addComponent(self.openButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+          .addComponent(self.refreshButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE))
+        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(self.saveButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+        .addComponent(self.unSaveButton, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+        .addContainerGap(20, sys.maxint))
+    )
+
+    layout = GroupLayout(self.getContentPane())
+    self.getContentPane().setLayout(layout)
+    layout.setHorizontalGroup(
+      layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+      .addComponent(self.bgPanel, GroupLayout.Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, sys.maxint)
+    )
+    layout.setVerticalGroup(
+      layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+      .addComponent(self.bgPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, sys.maxint)
+    )
+
+    self.pack()
+    self.setLocationRelativeTo(None)
     
-def unsaveButtonClicked(num):
-  numField3.delete(0, tkinter.END)
-  selected = reddit.submission(id = savedResults[num - 1].postID)
-  selected.unsave()
-  viewSaved()
+    with open("hotPosts.csv", "r") as f:
+      reader = csv.reader(f)
+      lis = list(reader)
+      
+    for i in lis:
+      self.postTable.getModel().addRow([i[0],
+                                        i[1],
+                                        i[2],
+                                        i[3]
+                                       ]
+                                      )
     
-def openSaveButtonClicked():
-  linkx = ""
-  webbrowser.open(linkx)
-
-mainWindow = tkinter.Tk()
-mainWindow.title("Reddit Front Page")
-
-refreshButton = tkinter.Button(mainWindow, text = "Refresh", command = viewPosts)
-refreshButton.pack()
-
-postsList = tkinter.Text(mainWindow, wrap="none")
-postsList.pack()
+  def exitButtonMouseClicked(self, evt):
+    sys.exit()
   
-numField = tkinter.Entry(mainWindow)
-numField.pack()
+  def saveButtonMouseEntered(self, evt):
+    self.saveButton.setBorder(border.LineBorder(Color.black))
+    
+  def saveButtonMouseExited(self, evt):
+    self.saveButton.setBorder(None)
+    
+  def openButtonMouseEntered(self, evt):
+    self.openButton.setBorder(border.LineBorder(Color.black))
+    
+  def openButtonMouseExited(self, evt):
+    self.openButton.setBorder(None)
+    
+  def unSaveButtonMouseEntered(self, evt):
+    self.unSaveButton.setBorder(border.LineBorder(Color.black))
+    
+  def unSaveButtonMouseExited(self, evt):
+    self.unSaveButton.setBorder(None)
+    
+  def refreshButtonMouseEntered(self, evt):
+    self.refreshButton.setBorder(border.LineBorder(Color.black))
+    
+  def refreshButtonMouseExited(self, evt):
+    self.refreshButton.setBorder(None)
+      
+  def bgPanelMouseDragged(self, evt):
+    x = evt.getXOnScreen()
+    y = evt.getYOnScreen()
 
-submitButton = tkinter.Button(mainWindow, text = "Go To Post", command = lambda: submitButtonClicked(int(numField.get())))
-submitButton.pack()
-  
-numField2 = tkinter.Entry(mainWindow)
-numField2.pack()
-
-saveButton = tkinter.Button(mainWindow, text = "Save Post", command = lambda: saveButtonClicked(int(numField2.get())))
-saveButton.pack()
-  
-numField3 = tkinter.Entry(mainWindow)
-numField3.pack()
-
-unsaveButton = tkinter.Button(mainWindow, text = "Unsave Post", command = lambda: unsaveButtonClicked(int(numField3.get())))
-unsaveButton.pack()
-
-openSaveButton = tkinter.Button(mainWindow, text = "Open Saved Page", command = openSaveButtonClicked)
-openSaveButton.pack()
-
-postsList2 = tkinter.Text(mainWindow, wrap="none")
-postsList2.pack()
-
-viewPosts()
-viewSaved()
-mainWindow.mainloop()
+    self.setLocation(x - mouseLoc[0], y - mouseLoc[1])
+      
+  def bgPanelMousePressed(self, evt):
+    del mouseLoc[:]
+    mouseLoc.append(evt.getX())
+    mouseLoc.append(evt.getY())
+    
+  def openButtonMouseClicked(self, evt):
+    postLink = str(self.postTable.getModel().getValueAt(self.postTable.getSelectedRow(), 2))
+    webbrowser.open(postLink)
+    
+if __name__ == "__main__":
+  mainWindow().setVisible(True)
